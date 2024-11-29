65c5b65e759f9457053e3b4df162616ab6e02016
Diana <135276904+Diana-Cl@users.noreply.github.com>
2024-11-29 18:14:35 +0330

    Update warp.py

--- warp.py
+++ warp.py
@@ -142,6 +142,7 @@ def toSingBox(tag, clean_ip, detour):
                 ],
                 "private_key": f"{data['private_key']}",
                 "peer_public_key": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
+                "persistent_keepalive_interval": 15,
                 "mtu": 1300,
                 "reserved": data["config"]["reserved"],
                 "detour": f"{detour}",
