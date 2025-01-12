07eb0149a34aaca1ea8fe62242b05760a15ea8d9
Diana <135276904+Diana-Cl@users.noreply.github.com>
2025-01-12 13:23:41 +0330

    Update spring.py

--- edge/spring.py
+++ edge/spring.py
@@ -5,7 +5,6 @@ import os
 import datetime
 import base64
 import json
-
 warp_cidr = [
     "162.159.192.0/24",
     "162.159.193.0/24",
@@ -16,15 +15,22 @@ warp_cidr = [
     "188.114.98.0/24",
     "188.114.99.0/24",
 ]
-
+# تنظیم مسیرهای اصلی
 script_directory = os.path.dirname(__file__)
-Bestip_path = os.path.join(script_directory, "Bestip.txt")
-result_path = os.path.join(script_directory, "result.csv")
-
+main_directory = os.path.dirname(script_directory)
+edge_directory = os.path.join(main_directory, "edge")
+# ایجاد فولدر edge اگر وجود نداشت
+if not os.path.exists(edge_directory):
+    os.makedirs(edge_directory)
+# مسیر فایل‌ها
+Bestip_path = os.path.join(edge_directory, "Bestip.txt")
+endpoints_path_edge = os.path.join(edge_directory, "endpoints.csv")
+endpoints_path_main = os.path.join(main_directory, "endpoints.csv")
+singbox_path = os.path.join(main_directory, "sing-box.json")
+warp_path = os.path.join(main_directory, "warp.json")
 def create_ips():
     c = 0
     top_ips = sum(len(list(ipaddress.IPv4Network(cidr))) for cidr in warp_cidr)
-
     with open(Bestip_path, "w") as file:
         for cidr in warp_cidr:
             ip_addresses = list(ipaddress.IPv4Network(cidr))
@@ -33,7 +39,7 @@ def create_ips():
                 file.write(str(addr))
                 if c != top_ips:
                     file.write("\n")
-
+# بررسی وجود فایل Bestip.txt
 if os.path.exists(Bestip_path):
     print("Bestip.txt exists.")
 else:
@@ -41,192 +47,42 @@ else:
     create_ips()
     print("Bestip.txt File Created Successfully!")
 
-def arch_suffix():
-    machine = platform.machine().lower()
-    if machine.startswith("i386") or machine.startswith("i686"):
-        return "386"
-    elif machine.startswith(("x86_64", "amd64")):
-        return "amd64"
-    elif machine.startswith(("armv8", "arm64", "aarch64")):
-        return "arm64"
-    elif machine.startswith("s390x"):
-        return "s390x"
-    else:
-        raise ValueError("Unsupported CPU architecture")
-
-arch = arch_suffix()
-
-print("Fetching warp program...")
-url = f"https://gitlab.com/Misaka-blog/warp-script/-/raw/main/files/warp-yxip/warp-linux-{arch}"
-
-subprocess.run(["wget", url, "-O", "warp"])
-os.chmod("warp", 0o755)
-command = "./warp >/dev/null 2>&1"
-print("Scanning IPs...")
-process = subprocess.Popen(command, shell=True)
-
-process.wait()
-
-if process.returncode != 0:
-    print("Error: Warp execution failed.")
-else:
-    print("Warp executed successfully.")
-
-# Check if result.csv exists
-if not os.path.exists(result_path):
-    print(f"Error: {result_path} does not exist.")
-    exit(1)
-
-Bestip = []
-
-with open(result_path, "r") as csv_file:
-    next(csv_file)
-    c = 0
-    for line in csv_file:
-        Bestip.append(line.split(",")[0])
-        c += 1
-        if c == 2:
-            break
-
-# Check if Bestip has at least 2 elements
-if len(Bestip) < 2:
-    print(f"Error: {result_path} does not contain enough data.")
-    exit(1)
-
-with open("Bestip.txt", "w") as f:
-    for ip in Bestip:
-        f.write(f"{ip}\n")
-
-formatted_time = datetime.datetime.now().strftime("%a, %d %b %Y, %H:%M")
-
-def export_Hiddify(t_ips):
-    config_prefix = (
-        f"warp://{t_ips[0]}?ifp=1-3&ifpm=m4#⚪️Tehran&&detour=warp://{t_ips[1]}?ifp=1-2&ifpm=m5#🟡Berlin"
-    )
-    return config_prefix, formatted_time
-
-title = (
-    "//profile-title: base64:"
-    + base64.b64encode("Freedom to Dream 💛✨".encode("utf-8")).decode("utf-8")
-    + "\n"
-)
-update_interval = "//profile-update-interval: 4\n"
-sub_info = "//subscription-userinfo: upload=805306368000; download=2576980377600; total=6012954214400; expire=1762677732\n"
-profile_web = "//profile-web-page-url: https://github.com/NiREvil\n"
-last_modified = "//last update on: " + formatted_time + "\n"
-
-config_prefix, _ = export_Hiddify(Bestip)
-with open("warp.json", "w") as op:
-    op.write(
-        title + update_interval + sub_info + profile_web + last_modified + config_prefix
-    )
-
-os.remove("warp")
-
-def toSingBox(tag, clean_ip, detour):
-    print("Generating Warp Conf")
-    command = 'wget -N "https://gitlab.com/fscarmen/warp/-/raw/main/api.sh" && sudo bash api.sh -r'
-    prc = subprocess.run(command, capture_output=True, text=True, shell=True)
-    output = prc.stdout
-
-    if (prc.returncode == 0) and output:
-        try:
-            data = json.loads(output)
-            wg = {
-                "tag": f"{tag}",
-                "type": "wireguard",
-                "server": f"{clean_ip.split(':')[0]}",
-                "server_port": int(clean_ip.split(":")[1]),
-                "local_address": [
-                    "172.16.0.2/32",
-                    "2606:4700:110:8735:bb29:91bc:1c82:aa73/128",
-                ],
-                "private_key": f"{data['private_key']}",
-                "peer_public_key": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
-                "mtu": 1300,
-                "reserved": data["config"]["reserved"],
-                "detour": f"{detour}",
-                "workers": 2,
-            }
-
-            for file in ["api.sh"]:
-                if os.path.exists(file):
-                    os.remove(file)
-                    print(f"Removed {file}")
-                else:
-                    print(f"{file} not found, skipping removal")
-
-            return wg
-        except json.JSONDecodeError:
-            print("Error: Unable to parse JSON output")
-            return None
-        except KeyError as e:
-            print(f"Error: Missing key in JSON data: {e}")
-            return None
-    else:
-        print("Error: Command execution failed or produced no output")
-        return None
-
-def export_SingBox(t_ips):
-    with open("edge/assets/singbox-template.json", "r") as f:
-        data = json.load(f)
-
-    data["outbounds"][1]["outbounds"].extend(["⚪️Tehran", "🟡Berlin"])
-
-    tehran_wg = toSingBox("⚪️Tehran", t_ips[0], "direct")
-    if tehran_wg:
-        data["outbounds"].insert(2, tehran_wg)
-    else:
-        print("Failed to generate ⚪️Tehran configuration")
-
-    berlin_wg = toSingBox("🟡Berlin", t_ips[1], "⚪️Tehran")
-    if berlin_wg:
-        data["outbounds"].insert(3, berlin_wg)
-    else:
-        print("Failed to generate 🟡Berlin configuration")
-
-    with open("sing-box.json", "w") as f:
-        json.dump(data, f, indent=4)
-
-def main(script_dir):
+def main(edge_dir, main_dir):
     try:
         arch = arch_suffix()
         print("Fetch warp program...")
         url = f"https://gitlab.com/Misaka-blog/warp-script/-/raw/main/files/warp-yxip/warp-linux-{arch}"
-        subprocess.run(["wget", url, "-O", "warp"], check=True)
-        os.chmod("warp", 0o755)
+        warp_executable = os.path.join(edge_dir, "warp")
+        subprocess.run(["wget", url, "-O", warp_executable], check=True)
+        os.chmod(warp_executable, 0o755)
         print("Scanning IPs...")
         subprocess.run(
-            ["./warp"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+            [warp_executable], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
         )
         print("Warp executed successfully.")
-
-        result_path = os.path.join(script_dir, "result.csv")
-
+        # کپی فایل endpoints.csv از edge به main
+        if os.path.exists(endpoints_path_edge):
+            with open(endpoints_path_edge, 'r') as src, open(endpoints_path_main, 'w') as dst:
+                dst.write(src.read())
     except subprocess.CalledProcessError as e:
         print(f"Error executing command: {e}")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
     finally:
-
-        if os.path.exists("warp"):
-            os.remove("warp")
-
+        if os.path.exists(warp_executable):
+            os.remove(warp_executable)
         top_ips = []
-        with open(result_path, "r") as csv_file:
+        with open(endpoints_path_edge, "r") as csv_file:
             next(csv_file)
             for _ in range(2):
                 line = next(csv_file, None)
                 if line:
                     top_ips.append(line.split(",")[0])
-
+        # ایجاد فایل‌های json در مسیر اصلی
         export_Hiddify(top_ips)
         export_SingBox(top_ips)
-
-        if os.path.exists(result_path):
-            os.remove(result_path)
-            os.remove(Bestip_path)
-
 if __name__ == "__main__":
     script_directory = os.path.dirname(__file__)
-    main(script_directory)
+    main_directory = os.path.dirname(script_directory)
+    edge_directory = os.path.join(main_directory, "edge")
+    main(edge_directory, main_directory)
