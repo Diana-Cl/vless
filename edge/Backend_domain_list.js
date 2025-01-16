8e9f384800d2bdf3182839e22e78114a8aa6a5cc
Diana <135276904+Diana-Cl@users.noreply.github.com>
2025-01-16 18:05:28 +0330

    Update Backend_domain_list.js

--- edge/Backend_domain_list.js
+++ edge/Backend_domain_list.js
@@ -1,3 +1,13 @@
+/**
+ * This script handles incoming HTTP requests and proxies them to a list of backend domains.
+ * - It parses the request URL and initializes a default list of backend domains.
+ * - If provided, it updates the backend domain list from an environment variable.
+ * - It attempts to forward the request to a randomly selected backend domain.
+ * - If the response from the backend domain matches the expected status code, it returns the response.
+ * - If all backend domains fail, it returns a 404 response.
+ * - It includes timeout functionality for backend requests and logs various details for debugging.
+ */
+
 export default {
 // Main function to handle incoming HTTP requests
 async fetch(request, env, ctx) {
@@ -7,15 +17,16 @@ const path = url.pathname;
 const params = url.search;
 // Default backend domain list
 let backendDomains = [
-'creativecommons.org',
-'sky.rethinkdns.com',
-'www.speedtest.net',
-'cdnjs.com',
-'www.wto.org',
-'zula.ir',
-'go.inmobi.com',
-'icook.hk',
-'fbi.gov'
+  'creativecommons.org',
+  'sky.rethinkdns.com',
+  'www.speedtest.net',
+  'go.inmobi.com',
+  'www.wto.org',
+  'cdnjs.com',
+  'icook.hk',
+  'zula.ir',
+  'fbi.gov',
+  'time.is'
 ];
 
 // If HOST exists in environment variables, get new backend domain list using ADD function
@@ -26,8 +37,8 @@ let testPath = env.PATH || '/';
 // Ensure test path starts with '/'
 if (testPath.charAt(0) !== '/') testPath = '/' + testPath;
 let responseCode = env.CODE || '200';
-// Log number of backend domains and their list
-console.log(`Backend count: ${backendDomains.length}\nBackend domains: ${backendDomains}\nTest path: ${testPath}\nResponse code: ${responseCode}`);
+  // Log number of backend domains and their list
+  console.log(`Backend count: ${backendDomains.length}\nBackend domains: ${backendDomains}\nTest path: ${testPath}\nResponse code: ${responseCode}`);
 
 // Store failed backend domains
 let failedBackends = [];
@@ -40,9 +51,9 @@ const controller = new AbortController();
 const id = setTimeout(() => controller.abort(), timeout);
 
 const response = await fetch(resource, {
-...options,
-signal: controller.signal
-}).finally(() => clearTimeout(id));
+  ...options,
+  signal: controller.signal
+  }).finally(() => clearTimeout(id));
 
 return response;
 }
@@ -53,35 +64,35 @@ async function getValidResponse(request, backendDomains) {
 while (backendDomains.length > 0) {
 // Randomly select a backend domain
 const randomBackend = backendDomains[Math.floor(Math.random() * backendDomains.length)];
-// Remove selected domain from the list
-backendDomains = backendDomains.filter(host => host !== randomBackend);
-
-url.hostname = randomBackend; // domain
-url.pathname = testPath.split('?')[0];
-url.search = testPath.split('?')[1] == "" ? "" : "?" + testPath.split('?')[1];
+  // Remove selected domain from the list
+  backendDomains = backendDomains.filter(host => host !== randomBackend);
+  
+  url.hostname = randomBackend; // domain
+  url.pathname = testPath.split('?')[0];
+  url.search = testPath.split('?')[1] == "" ? "" : "?" + testPath.split('?')[1];
 try {
 // Make request with timeout
 const response = await fetchWithTimeout(new Request(url), { timeout: 1618 });
-// If response status is 200, request is successful
-if (response.status.toString() == responseCode) {
-if (path != '/') url.pathname = path;
-console.log(`Using backend: ${url.hostname}`);
-//console.log(`Failed backends: ${failedBackends}`);
-console.log(`Remaining backends: ${backendDomains}`);
-url.search = params;
+  // If response status is 200, request is successful
+  if (response.status.toString() == responseCode) {
+  if (path != '/') url.pathname = path;
+  console.log(`Using backend: ${url.hostname}`);
+  //console.log(`Failed backends: ${failedBackends}`);
+  console.log(`Remaining backends: ${backendDomains}`);
+  url.search = params;
 return await fetch(new Request(url, request));
 } else {
-console.log(`Failed backend: ${url.hostname}:${response.status}`);
-}
+        console.log(`Failed backend: ${url.hostname}:${response.status}`);
+        }
 } catch (error) {
 // Catch request errors, add failed backend to the failed list
-failedBackends.push(randomBackend);
-}
+      failedBackends.push(randomBackend);
+      }
 }
 // If all backends fail, throw error
 return new Response('All backends are unavailable!', {
-status: 404,
-headers: { 'content-type': 'text/plain; charset=utf-8' },
+  status: 404,
+  headers: { 'content-type': 'text/plain; charset=utf-8' },
 });
 }
 
@@ -95,9 +106,9 @@ async function ADD(envadd) {
 // Then replace multiple consecutive commas with single comma
 var addtext = envadd.replace(/[ |"'\r\n]+/g, ',').replace(/,+/g, ',');
 
-// Remove leading and trailing commas (if any)
-if (addtext.charAt(0) == ',') addtext = addtext.slice(1);
-if (addtext.charAt(addtext.length - 1) == ',') addtext = addtext.slice(0, addtext.length - 1);
+  // Remove leading and trailing commas (if any)
+  if (addtext.charAt(0) == ',') addtext = addtext.slice(1);
+  if (addtext.charAt(addtext.length - 1) == ',') addtext = addtext.slice(0, addtext.length - 1);
 
 // Split string by comma to get address array
 const add = addtext.split(',');
