13c133de974ea9321ff43ea0eebd4feef1c67b40
Diana <135276904+Diana-Cl@users.noreply.github.com>
2025-02-27 22:45:56 +0330

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,17 @@ import globals from 'globals';
 
 export default [
   {
-    ignores: ['node_modules/**', 'dist/**', 'build/**', 'edge/waste/**', '.git/**', 'hiddify/**'],
+    ignores: [
+      'dist/**',
+      'build/**',
+      'warp.json',
+      'hiddify/**',
+      'edge/waste/**',
+      'node_modules/**',
+      '**/node_modules/**',
+      '**/package-lock.json'
+      'boringtun-boringtun-cli-0.5.2/**',
+    ],
   },
   js.configs.recommended,
   {
